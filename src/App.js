664097ce50a6cc7332fed2eb8a8c94157220984e
Luiz Eduardo R Figueiredo Paglioni <luizeduard0@iMac-de-Luiz.local>
2017-09-13 09:15:39 -0300

    delete button

--- src/App.js
+++ src/App.js
@@ -27,10 +27,16 @@ class App extends Component {
     ]
   }
 
+  removeContact = contact => {
+    this.setState((state) => ({
+      contacts: state.contacts.filter(c => c.id !== contact.id)
+    }))
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={this.state.contacts} />
+        <ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
       </div>
     );
   }
